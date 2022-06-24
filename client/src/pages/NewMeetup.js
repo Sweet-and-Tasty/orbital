f2cb86a7a67114fb091121a15ea934e47d7d00a8
hyperbola-bear <hung.hin.wang@gmail.com>
2022-06-24 21:43:57 +0800

    new form with axios and mongo

--- client/src/pages/NewMeetup.js
+++ client/src/pages/NewMeetup.js
@@ -3,35 +3,11 @@ import axios from "axios";
 
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-const NewMeetupPage = async () => {
-  const history = useNavigate();
-  function addMeetupHandler(meetupData) {
-    fetch(
-      "https://react-getting-started-a4afb-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
-      {
-        method: "POST",
-        body: JSON.stringify(meetupData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ).then(() => {
-      // can use async await
-      history.replace("/");
-    });
-    //const config = { headers: { "Content-Type": "application/json" } };
-    // try {
-    //  axios.post("api/event", meetupData, );
-    //  } catch (err) {
-    //   const errors = err.response.data.errors;
-    //  if (errors) {
-    //   errors.forEach((error) => { dispatch(setAlert(error.msg, "danger")); });
-  }
-
+const NewMeetupPage = () => {
   return (
     <section>
       <h1>Add New Course/ Class</h1>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+      <NewMeetupForm />
     </section>
   );
 };
