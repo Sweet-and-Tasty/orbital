ede4bc9db8ff730503628b7a850312453ca8f3c8
deenliong <deenliong@gmail.com>
2022-06-26 17:14:32 +0800

    remove alert button
    small little monkey

--- client/src/components/layout/Alert.jsx
+++ client/src/components/layout/Alert.jsx
@@ -1,23 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 const Alert = ({ alerts }) => (
-  <div className="alert-wrapper">
+  <div>
     {alerts.map((alert) => (
-      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-        {alert.msg}
-      </div>
+      <div key={alert.id}>{alert.msg}</div>
     ))}
   </div>
 );
 
 Alert.propTypes = {
-  alerts: PropTypes.array.isRequired,
+  alerts: PropTypes.array.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  alerts: state.alert,
+  alerts: state.alert
 });
 
 export default connect(mapStateToProps)(Alert);
