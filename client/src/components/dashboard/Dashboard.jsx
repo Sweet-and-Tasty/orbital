d669227300ab4780d2d2874608ef58b340f71fde
hyperbola-bear <hung.hin.wang@gmail.com>
2022-07-05 15:12:02 +0800

    put events from mongo onto calendar

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,108 @@
 // this file should no longer be here and should be using calendar instead
 
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import Calendar from "./Calendar";
+import moment from "moment";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
+import momentPlugin from "@fullcalendar/moment";
 
 const Dashboard = (props) => {
+  const [events, setEvents] = useState(INITIAL_EVENTS);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      let mapEvents = [];
+      const res = await axios.get("/api/event");
+      res.data.map((event) => {
+        mapEvents.push({
+          id: event._id,
+          title: event.title,
+          start: moment(event.startDateTime).format(),
+          //end: formatDate(event.end),
+        });
+        console.log(event.start);
+        console.log(moment(event.start).format());
+      });
+      console.log(res.data);
+      setEvents(mapEvents);
+    };
+    fetchEvents();
+    console.log(events);
+  }, []);
+
+  function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
+
+  const handleEventClick = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
   return (
     <div className="container mx-auto my-auto ">
       <h1>Welcome to dashboard,</h1>
-      <Calendar className="scale-75" />
+      <FullCalendar
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          momentPlugin,
+        ]}
+        titleFormat={"MMMM YYYY"}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        initialView="dayGridMonth"
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        //weekends={this.state.weekendsVisible}
+        events={events} // alternatively, use the `events` setting to fetch from a feed
+        select={handleDateSelect}
+        eventContent={renderEventContent} // custom render function
+        eventClick={handleEventClick}
+        // eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+        /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+      />
     </div>
   );
 };
