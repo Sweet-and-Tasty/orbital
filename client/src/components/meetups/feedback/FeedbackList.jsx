7219caa1968c3bb5ac1e985facf28a3443fb4223
deenliong <deenliong@gmail.com>
2022-07-24 21:49:33 +0800

    delete feedback working

--- client/src/components/meetups/feedback/FeedbackList.jsx
+++ client/src/components/meetups/feedback/FeedbackList.jsx
@@ -5,6 +5,7 @@ import classes from './Feedback.module.css';
 import FeedbackCard from '../../ui/FeedbackCard';
 // import FaTimes from 'react-icons/fa/FaTimes';
 // import FaEdit from 'react-icons/fa/FaEdit';
+import { FaBeer } from 'react-icons/fa';
 
 function FeedbackList() {
   let { id } = useParams();
@@ -26,6 +27,14 @@ function FeedbackList() {
     console.log(feedbackArray);
   }, []);
 
+  // deleteFeedback
+  const deleteFeedback = async (feedbackId) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      await axios.delete(`/api/event/feedback/${id}/${feedbackId}`);
+      window.location.reload();
+    }
+  };
+
   return (
     <>
       {/* if there is no feedback */}
@@ -39,13 +48,13 @@ function FeedbackList() {
             <ul className={classes.list}>
               <FeedbackCard>
                 <div className={classes.numDisplay}>{feedbackItem.rating}</div>
-                {/* <button
-                  onClick={() => deleteFeedback(item.id)}
+                <button
+                  onClick={() => deleteFeedback(feedbackItem._id)}
                   className="close"
                 >
-                  <FaTimes color="purple" />
+                  <FaBeer color="purple" />
                 </button>
-                <button onClick={() => editFeedback(item)} className="edit">
+                {/* <button onClick={() => editFeedback(item)} className="edit">
                   <FaEdit color="purple" />
                 </button> */}
                 <div className={classes.message}>{feedbackItem.text}</div>
