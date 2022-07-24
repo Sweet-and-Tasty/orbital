b553d13d6ade3ed0c12f9317763acf782b252d27
deenliong <deenliong@gmail.com>
2022-07-24 13:27:05 +0800

    Update PrivateRoute.js

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 const PrivateRoute = ({
   component: Component,
-  auth: { isAuthenticated, loading },
+  auth: { isAuthenticated, loading }
 }) => {
   if (!loading) if (isAuthenticated) return <Component />;
 
@@ -13,11 +13,11 @@ const PrivateRoute = ({
 };
 
 PrivateRoute.propTypes = {
-  auth: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
+  auth: state.auth
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
